f37cc361b25bb507559815514180d958912bc066
demonh1 <demon7987@gmail.com>
2016-04-10 19:23:58 +0300

    Update tray.cpp

--- tray.cpp
+++ tray.cpp
@@ -7,7 +7,6 @@ TRay::TRay(const TVector &point1, const TVector &point2)
 : _P(point1) {
 
 	_V = (point2.isUnit() ? point2 : TVector::unit(point2-point1));
-
 }
 
 
@@ -20,14 +19,10 @@ bool TRay::adjacentPoints(const TRay &ray, TVector &point1, TVector &point2) con
 		TVector tv; // Temporary vector to enable use of optimised routines
 
 		// Check for parallel rays
-		if (fabs(temp2)<EPSILON) {
-
+		if (fabs(temp2)< EPSILON) {
 			double mu = TVector::dot(_V, _P-ray._P)/temp;
-
 			point1 = _P;
-
 			TVector::add(ray._P, TVector::multiply(ray._V, mu, tv), point2);
-
 		} else {
 
 			double a = TVector::dot(_V, TVector::subtract(ray._P, _P, tv));
@@ -42,61 +37,42 @@ bool TRay::adjacentPoints(const TRay &ray, TVector &point1, TVector &point2) con
 		return true;
 	}
 	return false;
-
 }
-
-
 
 // Distance between two rays
 double TRay::dist(const TRay &ray) const {
-
 	TVector point1, point2;
 	if (adjacentPoints(ray, point1, point2))
-
 		return point1.dist(point2);
 
-	else
-		return 0.0;
-
+	return 0.0;
 }
 
 // Distance between a ray and a point
 double TRay::dist(const TVector &point) const {
 
 	if (isValid() && point.isValid()) {
-
 		TVector tv, point2;
 		double lambda = TVector::dot(_V, point-_P);
 		TVector::add(_P, TVector::multiply(_V, lambda, tv), point2);
-
 		return point.dist(point2);
 	}
 	return 0.0;
 }
 
-
 // Streaming
-std::ostream &TRay::write(std::ostream &out) const {
-
-	return isValid() ? out<<"(P="<<_P<<",V="<<_V<<")" : out<<"Invalid";
-
-}
-
+std::ostream &TRay::write(std::ostream &out) const { return isValid() ? out << "(P=" << _P << ",V=" << _V << ")" : out << "Invalid"; }
 
 std::istream &TRay::read(std::istream &in) {
-
 	char buf[20],ch;
 	in >> ch;
 
 	if (ch != 'I')
-		in >>ch>>ch>>_P>>ch>>ch>>ch>>_V;
+		in >> ch >> ch >>_P>>ch>>ch>>ch>>_V;
 
 	else {
-
 		*this=TRay();
 		in.get(buf,20,'d');
 	}
-
 	return in >> ch;
-
 }
